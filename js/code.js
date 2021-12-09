7eeae15376f4eae6fb68eae338710ca8e86e3f65
Greg Abram <gda@tacc.utexas.edu>
2021-12-09 13:47:01 -0700

    func name

--- js/code.js
+++ js/code.js
@@ -1,13 +1,14 @@
 
 var current_bibtex = "";
 
-function popup_bibtex(id) {
-  if (current_bibtex != "") popdown(current_bibtex);
-  e = document.getElementById(id);
-  if (e) { e.style.visibility = "visible"; current_bibtex = id; }
-}
 
 function popdown_bibtex(id) {
   e = document.getElementById(id);
   if (e) { e.style.visibility = "hidden"; current_bibtex = ""; }
+}
+
+function popup_bibtex(id) {
+  if (current_bibtex != "") popdown_bibtex(current_bibtex);
+  e = document.getElementById(id);
+  if (e) { e.style.visibility = "visible"; current_bibtex = id; }
 }
