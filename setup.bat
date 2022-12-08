38062b07a47e09058a9dcf55d2f1bc8c1f51964d
Kiet Tran <kietatr@gmail.com>
2022-12-08 02:39:09 -0600

    Update setup.bat to match with run_sass

--- setup.bat
+++ setup.bat
@@ -4,24 +4,18 @@
 
 @ECHO OFF
 
-:: Compile publications --------------------
-
 cmd /C python3 ./mkpubs > njk/publications.njk
-
-
-:: run_nunj --------------------------------
 
 node run_nunj
 
+:: Duplicating the functionality of file run_sass --------------------------------
 
-:: run_sass --------------------------------
-
-:: Create css/ if the folder doesn't exist
-IF NOT EXIST css\ (
-  MKDIR css
+IF NOT EXIST html\css\ (
+  MKDIR html\css
 )
 
-:: Run Sass
-cmd /C sass sass/style.scss css/style.css
+cmd /C sass sass/style.scss html/css/style.css
+
+:: End ----------------
 
 ECHO setup.bat has finished running. You now have 2 folders html/ and css/ filled with the website's latest content
